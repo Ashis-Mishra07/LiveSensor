8df1a2de4fb25e066629a570d370a645a8ef3a98
Ashis-Mishra07 <mishralucky074@gmail.com>
2025-05-26 12:46:22 +0530

    initial file added

---
+++ README.md
@@ -0,0 +1,10 @@
+# LiveSensor
+I want to create sendor fault prediction model .
+
+- Git Repo
+- Create Folder
+- Create virtual env
+- activate
+- create package
+- setup. and run
+- requirement file create and run
---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+from typing import List
+
+
+def get_requirements(file_path: str) -> List[str]:
+    requirements_list =  list[str] = []
+    return requirements_list
+
+
+setup(
+    name='sensor',
+    version='0.0.1',
+    author='Ashis',
+    author_email='mishralucky074@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements()
+)
