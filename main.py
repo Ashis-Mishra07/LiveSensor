d728313e13e1f674849f109940d5965cba8adbf7
Ashis-Mishra07 <mishralucky074@gmail.com>
2025-05-30 21:01:51 +0530

    data ingestion done .

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
+from sensor.configuration.mongo_db_connection import MongoDBClient
 from sensor.exception import SensorException
-import os
-import sys
-
+import os , sys
 from sensor.logger import logging
+from  sensor.utils import dump_csv_file_to_mongo_collection
+from sensor.entity.config_entity  import TrainingPipelineConfig,DataIngestionConfig
 
+from sensor.pipeline.training_pipeline import TrainPipeline
 
-def test_exception():
-    try:
-        # Simulating an error
-        logging.info("Testing exception handling")
-        1 / 0
-    except Exception as e:
-        raise SensorException(e, sys)
 
 if __name__ == "__main__":
-    try:
-        test_exception()
-    except Exception as e:
-        sensor_exception = SensorException(e, sys)
-        print(sensor_exception)  # This will print the custom error message with file name and line number
+
+    # file_path=r"F:\Complete ML\All_Projects\MLProject2\dataset.csv"
+    # database_name="ineuron"
+    # collection_name ="sensor"
+    # dump_csv_file_to_mongo_collection(file_path,database_name,collection_name)
+
+    training_pipeline = TrainPipeline()
+    # training_pipeline.start_data_ingestion()
+    training_pipeline.run_pipeline()
--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup, find_packages
 from typing import List
 
-
 def get_requirements(file_path: str) -> List[str]:
-    requirements_list : list[str] = []
+    requirements_list = []
+    with open(file_path) as file_obj:
+        requirements_list = file_obj.readlines()
+        requirements_list = [req.replace("\n", "") for req in requirements_list]
     return requirements_list
-
 
 setup(
     name='sensor',
@@ -13,5 +14,5 @@ setup(
     author='Ashis',
     author_email='mishralucky074@gmail.com',
     packages=find_packages(),
-    install_requires=get_requirements()
+    install_requires=get_requirements('requirements.txt')  # <-- pass the file here
 )
