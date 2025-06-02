9279e0782cfe531a84654297a69bfbc60da97dc8
Ashis-Mishra07 <mishralucky074@gmail.com>
2025-06-02 09:27:37 +0530

    fast api tutor

--- fast.py
+++ fast.py
@@ -4,7 +4,17 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/hello")
-async def root():
-    return {"message": "Hello, World!"}
+
+indian_places = {
+    'delhi': ['red fort', 'india gate', 'qutub minar'],
+    'mumbai': ['gateway of india', 'marine drive', 'elephanta caves'],
+    'jaipur': ['hawa mahal', 'amer fort', 'city palace'],
+    'varanasi': ['ghats', 'kashi vishwanath temple', 'sarnath'],
+    'goa': ['beaches', 'basilica of bom jesus', 'fort aguada'],
+}
+
+
+@app.get('/get_items/{place}')
+async def root(place):
+    return indian_places.get(place)
 
